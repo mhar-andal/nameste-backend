7d839efe04d04420400af984724a9d3a7e54faeb
Mhar Andal <mhar.andal@gamut.com>
2017-12-02 23:16:39 -0600

    adds some password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  attr_accessible :name, :password, :password_confirmation
+  has_secure_password
+
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+
 end
