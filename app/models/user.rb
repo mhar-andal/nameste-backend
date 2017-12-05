2d3373165a68bc29c804bca10000017fcf4066b3
Mhar Andal <mhar.andal@gamut.com>
2017-12-05 06:56:12 -0600

    remove attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  attr_accessible :name, :password, :password_confirmation
   has_secure_password
 
   validates :password, presence: true, confirmation: true, length: { minimum: 8 }
