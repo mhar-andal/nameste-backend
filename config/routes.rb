413ea9bdbb983c0ac3d2808dc59dc0feaba14693
Mark Richardson <mark@markeban.com>
2017-12-09 15:20:36 -0600

    added user and session authenticaion

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :v1 do
+    post '/users', to: 'users#create'
+    post '/sessions', to: 'sessions#create'
+    delete '/sessions', to: 'sessions#destroy'
+  end
 end
